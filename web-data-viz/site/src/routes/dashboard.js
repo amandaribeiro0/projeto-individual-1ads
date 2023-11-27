764c364353a71d1f3bc86feffc8b30d6c2519e8c
amandaribeiro1 <amanda.costa@sptech.school>
2023-11-27 16:01:41 -0300

    Adicionando rotas para grafico

--- web-data-viz/site/src/routes/dashboard.js
+++ web-data-viz/site/src/routes/dashboard.js
@@ -7,4 +7,12 @@ router.post("/buscarDados", function(req, res) {
     dashboardController.buscarDados(req, res)
 })
 
+router.post("/buscarTemporadas", function(req,res){
+    dashboardController.buscarTemporadas(req,res)
+})
+
+router.post("/buscarPersonagens", function(req,res){
+    dashboardController.buscarPersonagens(req,res)
+})
+
 module.exports = router
