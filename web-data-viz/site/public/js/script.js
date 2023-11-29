96c7df8d2099ab5122b26eb181fa8f01f876c276
amandaribeiro1 <amanda.costa@sptech.school>
2023-11-28 21:55:00 -0300

    Ajustes de estilo

--- web-data-viz/site/public/js/script.js
+++ web-data-viz/site/public/js/script.js
@@ -23,73 +23,102 @@ function sortearImagem() {
 }
 
 function sobreSerie() {
-    id_sobre.innerHTML = ` <h1>SOBRE A SÉRIE</h1>
-    <p>"Blindspot" é uma série de televisão repleta de ação, mistério e reviravoltas que cativou audiências em todo
-        o mundo. A trama gira em torno de uma mulher misteriosa chamada Jane Doe, que é encontrada em plena Times
-        Square, coberta de tatuagens intricadas e sem nenhuma memória de seu passado. Cada tatuagem em seu corpo
-        contém pistas para crimes e conspirações que a equipe de investigação, liderada por Kurt Weller, precisa
-        decifrar.
+    id_sobre.innerHTML = ` 
+    <h1>SOBRE A SÉRIE</h1>
+    <p class="headline">"Blindspot" é uma série de televisão repleta de ação, mistério e reviravoltas que cativou
+            audiências em todo
+            o mundo. A trama gira em torno de uma mulher misteriosa chamada Jane Doe, que é encontrada em plena Times
+            Square, coberta de tatuagens intricadas e sem nenhuma memória de seu passado. Cada tatuagem em seu corpo
+            contém pistas para crimes e conspirações que a equipe de investigação, liderada por Kurt Weller, precisa
+            decifrar.
 
-        À medida que a série se desenrola, os personagens principais enfrentam uma série de desafios que vão desde a
-        resolução de crimes complexos até a luta contra organizações secretas e ameaças globais. A narrativa é
-        repleta de suspense, ação e tensão, mantendo os espectadores à beira de seus assentos a cada episódio.
+            À medida que a série se desenrola, os personagens principais enfrentam uma série de desafios que vão desde a
+            resolução de crimes complexos até a luta contra organizações secretas e ameaças globais. A narrativa é
+            repleta de suspense, ação e tensão, mantendo os espectadores à beira de seus assentos a cada episódio.
 
-        Além das intrigantes tramas de investigação, "Blindspot" também explora temas como identidade, lealdade e
-        redenção. A personagem principal, Jane Doe, luta para descobrir sua verdadeira identidade e enfrenta as
-        consequências de seu passado misterioso. À medida que os relacionamentos se desenvolvem e segredos são
-        revelados, a série mergulha nas complexidades do lado humano de seus personagens.
+            Além das intrigantes tramas de investigação, "Blindspot" também explora temas como identidade, lealdade e
+            redenção. A personagem principal, Jane Doe, luta para descobrir sua verdadeira identidade e enfrenta as
+            consequências de seu passado misterioso. À medida que os relacionamentos se desenvolvem e segredos são
+            revelados, a série mergulha nas complexidades do lado humano de seus personagens.
 
-        Com um elenco talentoso, roteiro envolvente e ação de tirar o fôlego, "Blindspot" oferece uma experiência
-        única e emocionante para os fãs de séries de suspense e investigação. Ao longo de suas temporadas, a série
-        mantém os espectadores intrigados e ansiosos para desvendar os enigmas que cercam as tatuagens de Jane Doe e
-        os segredos que escondem. Com seu equilíbrio entre suspense e drama pessoal, "Blindspot" continua a ser uma
-        escolha popular para os amantes de mistério e intriga.
+            Com um elenco talentoso, roteiro envolvente e ação de tirar o fôlego, "Blindspot" oferece uma experiência
+            única e emocionante para os fãs de séries de suspense e investigação. Ao longo de suas temporadas, a série
+            mantém os espectadores intrigados e ansiosos para desvendar os enigmas que cercam as tatuagens de Jane Doe e
+            os segredos que escondem. Com seu equilíbrio entre suspense e drama pessoal, "Blindspot" continua a ser uma
+            escolha popular para os amantes de mistério e intriga.
 
-        Ao longo de todas as temporadas, "Blindspot" mantém uma mistura de ação, suspense, intrigas e drama pessoal,
-        mantendo os espectadores envolvidos na busca para decifrar as tatuagens de Jane e as complexidades dos
-        personagens e das tramas.  <br>
-        Conheça um pouco de cada temporada:
-        <br>  <br>
-        Temporada 1:
-        A primeira temporada introduz Jane Doe, uma mulher misteriosa coberta de tatuagens que é encontrada em Times
-        Square. Cada tatuagem contém pistas para crimes e conspirações que a equipe liderada por Kurt Weller começa
-        a investigar. A temporada explora a busca pela identidade de Jane, sua ligação com uma organização criminosa
-        chamada Sandstorm e os desafios de decifrar as tatuagens. A equipe enfrenta ameaças crescentes e segredos
-        surpreendentes.
-        <br>  <br>
-        Temporada 2:
-        Na segunda temporada, a equipe continua a desvendar os segredos por trás das tatuagens de Jane e a enfrentar
-        novos desafios. A história aprofunda os relacionamentos entre os personagens principais, enquanto eles
-        tentam deter Sandstorm e outras ameaças globais. A lealdade de Jane é questionada, e as consequências de
-        suas ações anteriores têm um impacto profundo na equipe.
-        <br>  <br>
-        Temporada 3:
-        A terceira temporada continua a saga de Jane e sua equipe, enquanto enfrentam conspirações, traições e lutas
-        internas. Novos personagens e vilões são introduzidos, ampliando o alcance da narrativa. A busca por
-        respostas sobre as origens de Jane e os objetivos da Sandstorm se torna ainda mais complexa.
-        <br>  <br>
-        Temporada 4:
-        Na quarta temporada, a equipe enfrenta uma nova ameaça e desvenda segredos relacionados a tecnologia de
-        inteligência artificial e espionagem. A temporada explora temas de privacidade e segurança cibernética,
-        enquanto a equipe trabalha para impedir uma catástrofe tecnológica.
-        <br>  <br>
-        Temporada 5:
-        A quinta e última temporada concentra-se na batalha final contra o perigoso grupo terrorista Sandstorm e nas
-        revelações finais sobre as origens de Jane e seu passado. A temporada culmina em um confronto épico que
-        decide o destino dos personagens e desvenda mistérios pendentes.
-    </p>
-    <br><br>
-    <a href="personagens.html" class="botao-sobre">Conheça os principais personagens</a>
-    <h3>Assista o trailer: </h3>
-    <iframe id="videoTrailer" width="460" height="215" src="https://www.youtube.com/embed/RqZkit-aCTo?si=S5JbUJTPvzBFOC5O"
-        title="YouTube video player" frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowfullscreen></iframe>`
+            Ao longo de todas as temporadas, "Blindspot" mantém uma mistura de ação, suspense, intrigas e drama pessoal,
+            mantendo os espectadores envolvidos na busca para decifrar as tatuagens de Jane e as complexidades dos
+            personagens e das tramas. <br>
+            Conheça um pouco de cada temporada:
+            <br> <br>
+        <div class="container-temporada">
+            <div class="box-temporada">
+                <h3>Temporada 1</h3>
+                A primeira temporada introduz Jane Doe, uma mulher misteriosa coberta de tatuagens que é encontrada em
+                Times
+                Square. Cada tatuagem contém pistas para crimes e conspirações que a equipe liderada por Kurt Weller
+                começa
+                a investigar. A temporada explora a busca pela identidade de Jane, sua ligação com uma organização
+                criminosa
+                chamada Sandstorm e os desafios de decifrar as tatuagens. A equipe enfrenta ameaças crescentes e
+                segredos
+                surpreendentes.
+            </div>
+
+            <div class="box-temporada">
+                <h3>Temporada 2</h3>
+                Na segunda temporada, a equipe continua a desvendar os segredos por trás das tatuagens de Jane e a
+                enfrentar
+                novos desafios. A história aprofunda os relacionamentos entre os personagens principais, enquanto eles
+                tentam deter Sandstorm e outras ameaças globais. A lealdade de Jane é questionada, e as consequências de
+                suas ações anteriores têm um impacto profundo na equipe.
+            </div>
+        </div>
+        <br><br>
+        <div class="container-temporada">
+            <div class="box-temporada">
+                <h3>Temporada 3</h3>
+                A terceira temporada continua a saga de Jane e sua equipe, enquanto enfrentam conspirações, traições e
+                lutas
+                internas. Novos personagens e vilões são introduzidos, ampliando o alcance da narrativa. A busca por
+                respostas sobre as origens de Jane e os objetivos da Sandstorm se torna ainda mais complexa.
+            </div>
+
+            <div class="box-temporada">
+                <h3>Temporada 4</h3>
+                Na quarta temporada, a equipe enfrenta uma nova ameaça e desvenda segredos relacionados a tecnologia de
+                inteligência artificial e espionagem. A temporada explora temas de privacidade e segurança cibernética,
+                enquanto a equipe trabalha para impedir uma catástrofe tecnológica.
+            </div>
+
+            <div class="box-temporada">
+                <h3>Temporada 5</h3>
+                A quinta e última temporada concentra-se na batalha final contra o perigoso grupo terrorista Sandstorm e
+                nas
+                revelações finais sobre as origens de Jane e seu passado. A temporada culmina em um confronto épico que
+                decide o destino dos personagens e desvenda mistérios pendentes.
+            </div>
+
+        </div>
+
+        </p>
+        <br><br>
+        <a href="personagens.html" class="botao-sobre">Conheça os principais personagens</a>
+        <h3>Assista o trailer: </h3><br>
+        <iframe id="videoTrailer" width="460" height="215"
+            src="https://www.youtube.com/embed/RqZkit-aCTo?si=S5JbUJTPvzBFOC5O" title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen></iframe>
+        <br><br>
+
+    `
 }
 
 function sobreSite() {
     id_sobre.innerHTML = `<h1>SOBRE O SITE</h1>
-    <p>Este site dedicado à série "Blindspot" foi desenvolvido como parte do curso de Análise e Desenvolvimento de
+    <p class="reveal">Este site dedicado à série "Blindspot" foi desenvolvido como parte do curso de Análise e Desenvolvimento de
         Sistemas na São Paulo Tech School. Nosso objetivo é aplicar o conhecimento adquirido na faculdade a um
         tópico de interesse pessoal, ao mesmo tempo em que relacionamos o projeto aos Objetivos de Desenvolvimento
         Sustentável (ODS) da ONU.
@@ -103,7 +132,7 @@ function sobreSite() {
     <br><br>
     <h2>Desenvolvido por:</h2>
     <h4>Amanda Ribeiro da Costa</h4>
-    <div class="sobreMim">
+    <div class="sobreMim reveal">
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium iusto earum nobis voluptatem
             perspiciatis atque, cumque magnam, maxime consectetur repellendus commodi, possimus corporis quibusdam
             dolor
